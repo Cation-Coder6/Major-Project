92b3e357081028d1e176de877a9fbea74a0f39a8
Cation-Coder6 <2005916@kiit.ac.in>
2023-10-10 16:21:24 +0530

    changed the default page

--- prompt-enhancer/index.js
+++ prompt-enhancer/index.js
@@ -17,7 +17,9 @@ app.use(bodyParser.json());
 app.use("/api/v1/db", promptRouter);
 app.use("/api/v1/suffix", suffixRouter);
 app.get("/", (req, res) => {
-  res.send("Welcome to Evil Meows Layer. This is the evil Prompt Enhancer.");
+  res.send(
+    "Welcome to Evil Meows Layer🐈🐈‍⬛. This is the evil Prompt Enhancer."
+  );
 });
 
 const port = process.env.PORT || 5000;
